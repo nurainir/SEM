b5e28127994d0d47a1712e4e505e93290ab4f88e
Nur Aini Rakhmawati <nurainir@gmail.com>
2019-05-03 15:33:46 +0700

    Add files via upload

---
+++ sop.R
@@ -0,0 +1,17 @@
+library(lavaan)
+
+datasop<-read.csv("./Otok3_SEMAMOS.csv")
+
+head(datasop)
+
+modelsop<-'X1 =~ X1.4 + X1.3 + X1.2 + X1.1
+            Y1 =~ Y1.1 + Y1.2 + Y1.3
+            Y2 =~ Y2.1 + Y2.2 + Y2.3
+            Y2 ~ X1 + Y1
+            Y1 ~ X1'
+
+fit <- sem(modelsop, data=datasop,meanstructure = TRUE)
+
+summary(fit, fit.measures=TRUE)
+
+standardizedSolution(fit)
